c76efc6fedd2fba4ca364f29858f01f3c12f1f85
carsten = <claxan-com@onlinehome.org>
2007-04-22 16:39:58 +0000

    * porting the static Makefile to the autotools to be able building the CDK
    git-svn-id: http://www.openmct.org/svn/branches/carsten@458 052c2572-4cfa-0310-8799-9599af202008

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,14 @@
 
 echo -e "Generating configuration for Open\033[1;35mM\033[1;33mC\033[1;32mT \033[0m..."
 
+echo "Processing autoreconf ...."
 autoreconf -f -i -s
+
+if [ $? -ne 0 ] 
+then
+    echo "Somthing went wrong while autoreconf runs ..."
+    exit
+fi
 
 echo "Done."
 echo "Now you can run './configure --help' if you don't know thats options are to set!"
