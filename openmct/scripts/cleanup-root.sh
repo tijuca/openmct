8a776c05a22fd5e86197eb9bd64de272deef7948
carsten = <claxan-com@onlinehome.org>
2008-02-24 10:14:23 +0000

    * fixed some little depencies in the startscripts on bootup   changed the filestructure of the rootfs   adding the recovery modul for the reset button
    git-svn-id: http://www.openmct.org/svn/trunk@569 052c2572-4cfa-0310-8799-9599af202008

--- openmct/scripts/cleanup-root.sh
+++ openmct/scripts/cleanup-root.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
+
+echo $1
+DIR=$1
 if [ "$1" != "" ]; then
 	rm -rf $1/home
 	rm -rf $1/mnt
 	rm -rf $1/opt
 	rm -rf $1/root
+	cd $1 && pwd  && ln -s /tmp/root root
 	rm -rf $1/usr
 	rm -rf $1/var
 	rm -rf $1/etc
 	rm $1/lib/libstdc++.so*
 	mkdir $1/usr
 	mkdir $1/var
-	mkdir $1/jffs2
-	mkdir $1/squashfs
-	mkdir $1/mnt
-	mkdir $1/harddisk
+	mkdir -p $1/media/hd
+	mkdir -p $1/media/usb1
+	mkdir -p $1/media/usb2
+	mkdir -p $1/media/nfs1
+	mkdir -p $1/media/nfs2
 fi
